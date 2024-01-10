a7a2cdeb6f056212c397d488cbbe33203232ddac
Oz <osvaldocanales4@gmail.com>
2024-01-10 10:29:36 -0700

    Added Roberta Model, now figure out a way to print whole database without it being to big

--- sentiment_analysis_project.py
+++ sentiment_analysis_project.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pprint import pprint
+#Import HuggingFace models 
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
 
 plt.style.use('ggplot')
 
@@ -27,7 +31,7 @@ file_path = "C:/Users/Creepy Weasel/Downloads/archive/Reviews.csv"
 df = pd.read_csv(file_path)
 
 #Limit dataframe to the first 500 rows 
-df = df.head(500)
+df = df.head(51)
 print(df.shape)
 
 '''
@@ -64,13 +68,13 @@ print(sia.polarity_scores('I am so happy!'))
 print(sia.polarity_scores('This is the worst thing ever!'))
 print(sia.polarity_scores(example))
 
+'''
 #Run the polarity score on the entire dataset 
 res = {}
 for i, row in tqdm(df.iterrows(), total = len(df)):
     text = row['Text']
     myid = row['Id']
     res[myid] = sia.polarity_scores(text)
-
 pprint(res)
 
 #Store into Pandas dataframe
@@ -85,7 +89,7 @@ print(vaders.dtypes)
 #Note to self: We use seaborn to integrate colosely with pandas data structures
 ax = sns.barplot(data = vaders, x = 'Score', y = 'compound')
 ax.set_title('Compound Score by Amazon Star review')
-plt.show()
+#plt.show()
 
 #Plot three subplots side by side that showcase the scores on the three sentimental scores
 fig, ax1 = plt.subplots(1, 3, figsize=(15, 5))
@@ -95,8 +99,72 @@ sns.barplot(data = vaders, x = 'Score', y = 'neg', ax=ax1[2])
 ax1[0].set_title('Positive')
 ax1[1].set_title('Neutral')
 ax1[2].set_title('Negative')
+#plt.tight_layout()
+#plt.show()
+'''
 
-plt.show()
+#Roberta Model that picks up on relationships between words (Sarcasim, context, ect..)
+#Pretrained model trained on 123.86 M tweets until the end of 2021
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+#Pretrained weights from the model
+#Note to self: Tokenizers translate text into data that can be processed by the model
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+
+def polarity_scores_roberta(example):
+    #Run for Roberta Model
+    encoded_text = tokenizer(example, return_tensors= 'pt')
+    output = model(**encoded_text)
+
+    #Convert raw predictions into regular scores
+    scores = output[0][0].detach().numpy()
+    #Convert predictions into probabilities that sum up to 1 
+    scores = softmax(scores)
+
+    scores_dict = { 
+    'roberta_neg' : scores[0],
+    'roberta_neu' : scores[1],
+    'roberta_pos' : scores[2]
+    }
+   
+    return scores_dict
+
+#Run the polarity score on the entire dataset using Roberta model
+res = {}
+for i, row in tqdm(df.iterrows(), total = len(df)):
+    try:
+        text = row['Text']
+        myid = row['Id']
+        #Vader results and rename
+        vader_result = sia.polarity_scores(text)
+        vader_result_rename = {}
+        for key, value in vader_result.items():
+            vader_result_rename[f"vader_{key}"] = value
+        #Roberta results 
+        roberta_result = polarity_scores_roberta(text)
+        both = {**vader_result_rename, ** roberta_result}
+        res[myid] = both
+    except RuntimeError:
+        print(f'Broke for id {myid}')
+    
+
+pprint(res)
+
+
+#Store into Pandas dataframe
+results_df = pd.DataFrame(res).T
+#Reset index and repece index with Id
+results_df = results_df.reset_index().rename(columns={'index': 'Id'})
+#Merge scores onto df dataframe 
+results_df = results_df.merge(df, how = 'left')
+print(results_df)
+
+
+
+
+
+
 
 
     
